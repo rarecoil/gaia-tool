dfa43e91952a42003f36f2d50b343abca3761e21
Laurens Holst <laurens.hg@grauw.nl>
2010-09-26 01:42:10 +0200

    Make message class for identity reply message.

--- src/main/java/nl/grauw/gaia_tool/ResponseReceiver.java
+++ src/main/java/nl/grauw/gaia_tool/ResponseReceiver.java
@@ -1,23 +1,36 @@
 package nl.grauw.gaia_tool;
 
+import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.Receiver;
+import javax.sound.midi.SysexMessage;
 
 import nl.grauw.gaia_tool.messages.GenericMessage;
+import nl.grauw.gaia_tool.messages.IdentityReplyMessage;
 
 public class ResponseReceiver implements Receiver {
 
 	@Override
 	public void send(MidiMessage message, long timeStamp) {
 		System.out.println("MIDI message received at " + timeStamp + ":");
-		System.out.println("* " + processMidiMessage(message));
+		try {
+			System.out.println("* " + processMidiMessage(message));
+		} catch(Exception e) {
+			System.out.println("*** Error: " + e);
+		}
 	}
 
 	@Override
 	public void close() {
 	}
 	
-	public MidiMessage processMidiMessage(MidiMessage message) {
+	public MidiMessage processMidiMessage(MidiMessage message) throws InvalidMidiDataException {
+		if (message instanceof SysexMessage) {
+			SysexMessage sem = (SysexMessage) message;
+			if (sem.getData()[2] == 6 && sem.getData()[3] == 2) {
+				return new IdentityReplyMessage(sem);
+			}
+		}
 		return new GenericMessage(message);
 	}
 
---
+++ src/main/java/nl/grauw/gaia_tool/messages/IdentityReplyMessage.java
@@ -0,0 +1,64 @@
+package nl.grauw.gaia_tool.messages;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.SysexMessage;
+
+public class IdentityReplyMessage extends SysexMessage {
+
+	public IdentityReplyMessage(SysexMessage sem) throws InvalidMidiDataException {
+		super();
+		byte[] data = sem.getData();
+		setMessage(sem.getStatus(), data, data.length);
+	}
+	
+	public int getDeviceId() {
+		return (int)(getData()[1] & 0xFF);
+	}
+	
+	public int getIdNumber() {
+		return (int)(getData()[4] & 0xFF);
+	}
+	
+	public int[] getDeviceFamilyCode() {
+		int[] rv = {
+				(int)(getData()[5] & 0xFF),
+				(int)(getData()[6] & 0xFF)
+			};
+		return rv;
+	}
+	
+	public int[] getDeviceFamilyNumberCode() {
+		int[] rv = {
+				(int)(getData()[7] & 0xFF),
+				(int)(getData()[8] & 0xFF)
+			};
+		return rv;
+	}
+	
+	public int[] getSoftwareRevisionLevel() {
+		int[] rv = {
+				(int)(getData()[9] & 0xFF),
+				(int)(getData()[10] & 0xFF),
+				(int)(getData()[11] & 0xFF),
+				(int)(getData()[12] & 0xFF)
+			};
+		return rv;
+	}
+	
+	protected static String toHex(int number) {
+		return String.format("%1$02XH", number);
+	}
+	
+	public String toString() {
+		String s = "Identity reply message. Device ID: " + toHex(getDeviceId());
+		s += ". ID number: " + toHex(getIdNumber());
+		int[] dfc = getDeviceFamilyCode();
+		s += ". Device family code: " + toHex(dfc[0]) + " " + toHex(dfc[1]);
+		int[] dfnc = getDeviceFamilyNumberCode();
+		s += ". Device family number code: " + toHex(dfnc[0]) + " " + toHex(dfnc[1]);
+		int[] srl = getSoftwareRevisionLevel();
+		s += ". Software revision level: " + srl[0] + "." + srl[1] + "." + srl[2] + "." + srl[3] + ".";
+		return s;
+	}
+	
+}
