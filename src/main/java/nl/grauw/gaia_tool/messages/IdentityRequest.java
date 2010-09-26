91c2cf305bf3e99023612f07354847f6655f9750
Laurens Holst <laurens.hg@grauw.nl>
2010-09-26 02:29:46 +0200

    Ah, device 0x7F is a broadcast device ID, split the constructors to reflect that better.

--- src/main/java/nl/grauw/gaia_tool/messages/IdentityRequest.java
+++ src/main/java/nl/grauw/gaia_tool/messages/IdentityRequest.java
@@ -4,21 +4,25 @@ import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.SysexMessage;
 
 public class IdentityRequest extends SysexMessage {
-
+	
+	final static int BROADCAST_DEVICE = 0x7F;
+	
 	/**
-	 * Construct an identity request message with device ID 0x7F.
+	 * Construct a broadcast identity request message.
 	 */
 	public IdentityRequest() throws InvalidMidiDataException {
-		this(0x7F);
+		super();
+		byte[] message = {0x7E, BROADCAST_DEVICE, 0x06, 0x01, (byte)0xF7};
+		setMessage(SYSTEM_EXCLUSIVE, message, message.length);
 	}
 	
 	/**
-	 * Construct an identity request message with nonstandard device ID.
-	 * @param device_id The device ID. Must be between 0x10-0x1F (inclusive), or 0x7F.
+	 * Construct an identity request message for a specific device.
+	 * @param device_id The device ID. Must be between 0x10-0x1F (inclusive).
 	 */
 	public IdentityRequest(int device_id) throws InvalidMidiDataException {
 		super();
-		if ((device_id < 0x10 || device_id > 0x1F) && device_id != 0x7F)
+		if (device_id < 0x10 || device_id > 0x1F)
 			throw new InvalidMidiDataException("Device ID out of range.");
 		
 		byte[] message = {0x7E, (byte)device_id, 0x06, 0x01, (byte)0xF7};
