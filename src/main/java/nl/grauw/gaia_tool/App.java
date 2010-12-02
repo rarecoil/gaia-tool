84750a4b6574bbd502dc3f1235b745014cd0aa02
Laurens Holst <laurens.hg@grauw.nl>
2010-12-02 02:42:56 +0100

    Separate out the window stuff into a view class.

--- src/main/java/nl/grauw/gaia_tool/App.java
+++ src/main/java/nl/grauw/gaia_tool/App.java
@@ -1,127 +1,21 @@
 package nl.grauw.gaia_tool;
 
-import java.awt.Frame;
-import java.awt.Menu;
-import java.awt.MenuBar;
-import java.awt.MenuItem;
-import java.awt.TextArea;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.util.Observable;
-import java.util.Observer;
-
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiUnavailableException;
 
-public class App extends Frame implements WindowListener, ActionListener, Observer {
+import nl.grauw.gaia_tool.views.GaiaView;
+
+public class App {
 	
-	private static final long serialVersionUID = 4950285236879118899L;
-	
-	private Gaia gaia;
-	
-	private MenuItem playItem;
-	private MenuItem gm1SystemOn;
-	private MenuItem gm2SystemOn;
-	private MenuItem gmSystemOff;
-	private MenuItem systemDataRequest;
-	private MenuItem patchDataRequest;
-	private TextArea log;
-	
-	public App() throws MidiUnavailableException, InvalidMidiDataException {
-		super();
-		
-		gaia = new Gaia();
+	public static void main(String[] args) throws MidiUnavailableException, InvalidMidiDataException {
+		// instantiate model
+		Gaia gaia = new Gaia();
 		gaia.open();
 		gaia.requestIdentity();
 		
-		// initialise UI
-		addWindowListener(this);
-		setTitle("Roland GAIA SH-01 tool — by Grauw");
-		setSize(800, 700);
-		MenuBar mb = new MenuBar();
-		Menu testMenu = new Menu("Test");
-		playItem = new MenuItem("Play test notes");
-		playItem.addActionListener(this);
-		testMenu.add(playItem);
-		gm1SystemOn = new MenuItem("GM system on");
-		gm1SystemOn.addActionListener(this);
-		testMenu.add(gm1SystemOn);
-		gm2SystemOn = new MenuItem("GM2 system on");
-		gm2SystemOn.addActionListener(this);
-		testMenu.add(gm2SystemOn);
-		gmSystemOff = new MenuItem("GM system off");
-		gmSystemOff.addActionListener(this);
-		testMenu.add(gmSystemOff);
-		systemDataRequest = new MenuItem("System data request");
-		systemDataRequest.addActionListener(this);
-		testMenu.add(systemDataRequest);
-		patchDataRequest = new MenuItem("Patch data request");
-		patchDataRequest.addActionListener(this);
-		testMenu.add(patchDataRequest);
-		mb.add(testMenu);
-		setMenuBar(mb);
-		log = new TextArea("", 24, 80, TextArea.SCROLLBARS_VERTICAL_ONLY);
-		add(log);
-		
-		// observe models
-		gaia.getLog().addObserver(this);
-	}
-
-	public static void main(String[] args) throws MidiUnavailableException, InvalidMidiDataException {
-		App app = new App();
-		app.setVisible(true);
-	}
-	
-	public void windowClosing(WindowEvent e) {
-		try {
-			gaia.close();
-		} catch (MidiUnavailableException e1) {
-			e1.printStackTrace();
-		}
-		dispose();
-	}
-
-	// unused window events
-	public void windowOpened(WindowEvent e) {}
-	public void windowClosed(WindowEvent e) {}
-	public void windowIconified(WindowEvent e) {}
-	public void windowDeiconified(WindowEvent e) {}
-	public void windowActivated(WindowEvent e) {}
-	public void windowDeactivated(WindowEvent e) {}
-
-	public void actionPerformed(ActionEvent e) {
-		try {
-			if (e.getSource() == playItem) {
-				gaia.playTestNote();
-				gaia.playGMTestNote();
-				gaia.playGM2TestNote();
-			} else if (e.getSource() == gm1SystemOn) {
-				gaia.sendGM1SystemOn();
-			} else if (e.getSource() == gm2SystemOn) {
-				gaia.sendGM2SystemOn();
-			} else if (e.getSource() == gmSystemOff) {
-				gaia.sendGMSystemOff();
-			} else if (e.getSource() == systemDataRequest) {
-				gaia.sendSystemDataRequest();
-			} else if (e.getSource() == patchDataRequest) {
-				gaia.sendPatchDataRequest();
-			}
-		} catch (InvalidMidiDataException e1) {
-			e1.printStackTrace();
-		}
-	}
-
-	@Override
-	public void update(Observable o, Object arg) {
-		if (o instanceof Log) {
-			update((Log) o, arg);
-		}
-	}
-	
-	public void update(Log l, Object arg) {
-		log.append(l.getLog().substring(log.getText().length()));
+		// render view
+		GaiaView view = new GaiaView(gaia);
+		view.setVisible(true);
 	}
 	
 }
