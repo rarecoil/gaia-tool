a3ed3a9f4ff792d1d56f53f00c5e54517f034c12
Laurens Holst <laurens.hg@grauw.nl>
2010-12-08 17:04:10 +0100

    Correct constant names.

--- src/main/java/nl/grauw/gaia_tool/messages/GM1SystemOn.java
+++ src/main/java/nl/grauw/gaia_tool/messages/GM1SystemOn.java
@@ -5,10 +5,10 @@ import javax.sound.midi.InvalidMidiDataException;
 public class GM1SystemOn extends UniversalSysex {
 	
 	final static byte GENERAL_MIDI_MESSAGE = 0x09;
-	final static byte GENERAL_MIDI_OFF = 0x01;
+	final static byte GENERAL_MIDI_ON = 0x01;
 	
 	public GM1SystemOn() throws InvalidMidiDataException {
-		super(GENERAL_MIDI_MESSAGE, GENERAL_MIDI_OFF);
+		super(GENERAL_MIDI_MESSAGE, GENERAL_MIDI_ON);
 	}
 	
 }
--- src/main/java/nl/grauw/gaia_tool/messages/GM2SystemOn.java
+++ src/main/java/nl/grauw/gaia_tool/messages/GM2SystemOn.java
@@ -5,10 +5,10 @@ import javax.sound.midi.InvalidMidiDataException;
 public class GM2SystemOn extends UniversalSysex {
 	
 	final static byte GENERAL_MIDI_MESSAGE = 0x09;
-	final static byte GENERAL_MIDI_OFF = 0x03;
+	final static byte GENERAL_MIDI_2_ON = 0x03;
 	
 	public GM2SystemOn() throws InvalidMidiDataException {
-		super(GENERAL_MIDI_MESSAGE, GENERAL_MIDI_OFF);
+		super(GENERAL_MIDI_MESSAGE, GENERAL_MIDI_2_ON);
 	}
 	
 }
