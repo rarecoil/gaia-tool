1a4aa5643ea034029e0535e044b96c50515e81a5
Laurens Holst <laurens.hg@grauw.nl>
2010-12-12 23:51:46 +0100

    Return a Note object for PatchArpeggioPatternParameters::getOriginalNote().

--- src/main/java/nl/grauw/gaia_tool/parameters/PatchArpeggioPatternParameters.java
+++ src/main/java/nl/grauw/gaia_tool/parameters/PatchArpeggioPatternParameters.java
@@ -1,5 +1,6 @@
 package nl.grauw.gaia_tool.parameters;
 
+import nl.grauw.gaia_tool.Note;
 import nl.grauw.gaia_tool.Value;
 
 public class PatchArpeggioPatternParameters extends Parameters {
@@ -11,8 +12,13 @@ public class PatchArpeggioPatternParameters extends Parameters {
 			throw new RuntimeException("Address map size mismatch.");
 	}
 	
-	public Value getOriginalNote() {
-		return new Value(addressMap[0x00] << 4 | addressMap[0x01], 0, 128);
+	/**
+	 * Get the pattern’s original note (C-4 is the base).
+	 * Note number 128 (G#9) means OFF.
+	 * @return The pattern original note.
+	 */
+	public Note getOriginalNote() {
+		return new Note(addressMap[0x00] << 4 | addressMap[0x01]);
 	}
 	
 	public Value getStepData(int step) {
@@ -30,7 +36,7 @@ public class PatchArpeggioPatternParameters extends Parameters {
 		}
 		
 		return "Patch arpeggio pattern parameters:\n" +
-				String.format("Original note: %s\n", getOriginalNote()) +
+				String.format("Original note: %s\n", getOriginalNote().getNoteNumber() != 129 ? getOriginalNote() : "OFF") +
 				String.format("Step data: %s\n", stepData);
 	}
 	
