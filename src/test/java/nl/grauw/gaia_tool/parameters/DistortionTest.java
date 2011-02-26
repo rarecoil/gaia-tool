8cf75d128b57e5199c876e38cf045612a216762b
Laurens Holst <laurens.hg@grauw.nl>
2011-02-26 19:39:08 +0100

    Improve test for Distortion::updateParameters() by comparing with actual GAIA output.

--- src/test/java/nl/grauw/gaia_tool/parameters/DistortionTest.java
+++ src/test/java/nl/grauw/gaia_tool/parameters/DistortionTest.java
@@ -44,22 +44,6 @@ public class DistortionTest {
 	}
 	
 	@Test
-	public void testUpdateParameters_Control_1() throws InvalidMidiDataException {
-		Distortion parameters = getTestParameters();
-		ControlChangeMessage cc = new ControlChangeMessage(0, Controller.DISTORTION_CONTROL_1, 47);
-		parameters.updateParameters(cc);
-		assertEquals(47, parameters.getMFXParameter(2).getValue());
-	}
-	
-	@Test
-	public void testUpdateParameters_Level() throws InvalidMidiDataException {
-		Distortion parameters = getTestParameters();
-		ControlChangeMessage cc = new ControlChangeMessage(0, Controller.DISTORTION_LEVEL, 47);
-		parameters.updateParameters(cc);
-		assertEquals(47, parameters.getMFXParameter(1).getValue());
-	}
-	
-	@Test
 	public void testGetDistortionType() {
 		Distortion pdp = getTestParameters();
 		assertEquals(DistortionType.BIT_CRASH, pdp.getDistortionType());
@@ -144,6 +128,89 @@ public class DistortionTest {
 		pdp.getDistortionTypeValue().setValue(DistortionType.BIT_CRASH);
 		pdp.getMFXParameter(4).setValue(47);
 		assertEquals(47, pdp.getFilter().getValue());
+	}
+	
+	@Test
+	public void testUpdateParameters_Level() throws InvalidMidiDataException {
+		Distortion parameters = getTestParameters();
+		int[] expected = {
+				  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,
+				 16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
+				 32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
+				 48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
+				 64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
+				 80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
+				 96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+				112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127
+			};
+		for (int i = 0; i < 128; i++) {
+			ControlChangeMessage cc = new ControlChangeMessage(0, Controller.DISTORTION_LEVEL, i);
+			parameters.updateParameters(cc);
+			assertEquals(expected[i], parameters.getLevel().getValue());
+		}
+	}
+	
+	@Test
+	public void testUpdateParameters_Control_1_Distortion() throws InvalidMidiDataException {
+		Distortion parameters = getTestParameters();
+		parameters.getDistortionTypeValue().setValue(DistortionType.DIST);
+		int[] expected = {
+				  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,
+				 16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
+				 32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
+				 48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
+				 64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
+				 80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
+				 96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+				112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127
+			};
+		for (int i = 0; i < 128; i++) {
+			ControlChangeMessage cc = new ControlChangeMessage(0, Controller.DISTORTION_CONTROL_1, i);
+			parameters.updateParameters(cc);
+			assertEquals(expected[i], parameters.getDrive().getValue());
+		}
+	}
+	
+	@Test
+	public void testUpdateParameters_Control_1_Fuzz() throws InvalidMidiDataException {
+		Distortion parameters = getTestParameters();
+		parameters.getDistortionTypeValue().setValue(DistortionType.FUZZ);
+		int[] expected = {
+				  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,
+				 16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
+				 32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
+				 48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
+				 64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
+				 80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
+				 96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+				112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127
+			};
+		for (int i = 0; i < 128; i++) {
+			ControlChangeMessage cc = new ControlChangeMessage(0, Controller.DISTORTION_CONTROL_1, i);
+			parameters.updateParameters(cc);
+			assertEquals(expected[i], parameters.getDrive().getValue());
+		}
+	}
+	
+	@Test
+	public void testUpdateParameters_Control_1_Bit_Crash() throws InvalidMidiDataException {
+		Distortion parameters = getTestParameters();
+		parameters.getDistortionTypeValue().setValue(DistortionType.BIT_CRASH);
+		int[] expected = {
+				  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,
+				 16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
+				 32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
+				 48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
+				 64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
+				 80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
+				 96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+				112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127
+			};
+		for (int i = 0; i < 128; i++) {
+			ControlChangeMessage cc = new ControlChangeMessage(0, Controller.DISTORTION_CONTROL_1, i);
+			parameters.updateParameters(cc);
+			assertEquals(expected[i], parameters.getSampleRate().getValue());
+		}
 	}
 
 }
