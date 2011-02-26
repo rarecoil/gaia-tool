ee3d0be9ba9aefa01e688d7715f31663d32a2669
Laurens Holst <laurens.hg@grauw.nl>
2011-02-26 19:41:42 +0100

    Improve tests for Revern::updateParameters() by comparing with actual GAIA output.

--- src/test/java/nl/grauw/gaia_tool/parameters/ReverbTest.java
+++ src/test/java/nl/grauw/gaia_tool/parameters/ReverbTest.java
@@ -36,22 +36,6 @@ public class ReverbTest {
 	public void testPatchReverbParameters() {
 		new Reverb(testAddress, new byte[80]);
 	}
-	
-	@Test
-	public void testUpdateParameters_Control_1() throws InvalidMidiDataException {
-		Reverb parameters = getTestParameters();
-		ControlChangeMessage cc = new ControlChangeMessage(0, Controller.REVERB_CONTROL_1, 47);
-		parameters.updateParameters(cc);
-		assertEquals(47, parameters.getReverbParameter(2).getValue());
-	}
-	
-	@Test
-	public void testUpdateParameters_Level() throws InvalidMidiDataException {
-		Reverb parameters = getTestParameters();
-		ControlChangeMessage cc = new ControlChangeMessage(0, Controller.REVERB_LEVEL, 47);
-		parameters.updateParameters(cc);
-		assertEquals(47, parameters.getReverbParameter(1).getValue());
-	}
 
 	@Test
 	public void testGetReverbType() {
@@ -82,6 +66,46 @@ public class ReverbTest {
 	public void testGetReverbParameterInvalidHigh() {
 		Reverb prp = getTestParameters();
 		prp.getReverbParameter(21);
+	}
+	
+	@Test
+	public void testUpdateParameters_Level() throws InvalidMidiDataException {
+		Reverb parameters = getTestParameters();
+		int[] expected = {
+				  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,
+				 16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
+				 32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
+				 48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
+				 64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
+				 80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
+				 96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+				112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127
+			};
+		for (int i = 0; i < 128; i++) {
+			ControlChangeMessage cc = new ControlChangeMessage(0, Controller.REVERB_LEVEL, i);
+			parameters.updateParameters(cc);
+			assertEquals(expected[i], parameters.getLevel().getValue());
+		}
+	}
+	
+	@Test
+	public void testUpdateParameters_Control_1() throws InvalidMidiDataException {
+		Reverb parameters = getTestParameters();
+		int[] expected = {
+				  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,
+				 16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
+				 32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
+				 48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
+				 64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
+				 80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
+				 96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+				112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127
+			};
+		for (int i = 0; i < 128; i++) {
+			ControlChangeMessage cc = new ControlChangeMessage(0, Controller.REVERB_CONTROL_1, i);
+			parameters.updateParameters(cc);
+			assertEquals(expected[i], parameters.getTime().getValue());
+		}
 	}
 
 }
